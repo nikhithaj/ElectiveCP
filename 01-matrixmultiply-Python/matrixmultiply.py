cf99e2aed6e3fb5f90bba7d33a18339caf9301e7
nikhithaj <nikhithajonnakuti400@msitprogram.net>
2021-07-21 22:46:09 +0530

    commit

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,7 +5,17 @@
 
 
 def fun_matrixmultiply(m1, m2):
-    return None
+    
+    # your code goes here
+    if(len(m1[0])!=len(m2)):
+        return(None)
+    res=[[0 for i in range(len(m2[0]))] for j in range(len(m1))]
+    for i in range(len(m1)):
+        for j in range(len(m2[0])):
+            for k in range(len(m2)):
+                res[i][j]+=m1[i][k]*m2[k][j]
+    return(res)
+    
 
 
 
